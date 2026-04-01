6f3bfeac92358999b694bd20528bd79e6e611bf1
rabiaquantristech <rabiaquantristech@gmail.com>
2026-04-01 11:56:44 +0500

    add old

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
 const User = require("../models/userModel.js");
 
 passport.serializeUser((user, done) => {
@@ -15,13 +16,13 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-// ================= GOOGLE ONLY =================
+// ================= GOOGLE =================
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.BACKEND_BASE_URL}/api/auth/google/callback`,
+      callbackURL: "/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -44,164 +45,35 @@ passport.use(
   )
 );
 
+// ================= LINKEDIN =================
+passport.use(
+  new LinkedInStrategy(
+    {
+      clientID: process.env.LINKEDIN_CLIENT_ID,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+      callbackURL: "/api/auth/linkedin/callback",
+      scope: ["r_emailaddress", "r_liteprofile"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails[0].value;
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            name: profile.displayName,
+            email,
+            isVerified: true,
+            password: "oauth_linkedin",
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
 module.exports = passport;
-
-// const passport = require("passport");
-// const GoogleStrategy = require("passport-google-oauth20").Strategy;
-// const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
-// const User = require("../models/userModel.js");
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(async (id, done) => {
-//   try {
-//     const user = await User.findById(id);
-//     done(null, user);
-//   } catch (err) {
-//     done(err, null);
-//   }
-// });
-
-// // ================= GOOGLE =================
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: `${process.env.BACKEND_BASE_URL}/api/auth/google/callback`, // FULL URL
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         let user = await User.findOne({ email: profile.emails[0].value });
-
-//         if (!user) {
-//           user = await User.create({
-//             name: profile.displayName,
-//             email: profile.emails[0].value,
-//             isVerified: true,
-//             password: "oauth_google",
-//           });
-//         }
-
-//         return done(null, user);
-//       } catch (err) {
-//         return done(err, null);
-//       }
-//     }
-//   )
-// );
-
-// // ================= LINKEDIN =================
-// passport.use(
-//   new LinkedInStrategy(
-//     {
-//       clientID: process.env.LINKEDIN_CLIENT_ID,
-//       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-//       callbackURL: `${process.env.BACKEND_BASE_URL}/api/auth/linkedin/callback`,
-//       scope: ["openid", "profile", "email"],
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         const email = profile.emails?.[0]?.value || profile.email;
-//         let user = await User.findOne({ email });
-
-//         if (!user) {
-//           user = await User.create({
-//             name: profile.displayName,
-//             email,
-//             isVerified: true,
-//             password: "oauth_linkedin",
-//           });
-//         }
-
-//         return done(null, user);
-//       } catch (err) {
-//         return done(err, null);
-//       }
-//     }
-//   )
-// );
-
-// module.exports = passport;
-
-// const passport = require("passport");
-// const GoogleStrategy = require("passport-google-oauth20").Strategy;
-// const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
-// const User = require("../models/userModel.js");
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(async (id, done) => {
-//   try {
-//     const user = await User.findById(id);
-//     done(null, user);
-//   } catch (err) {
-//     done(err, null);
-//   }
-// });
-
-// // ================= GOOGLE =================
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: "/api/auth/google/callback",
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         let user = await User.findOne({ email: profile.emails[0].value });
-
-//         if (!user) {
-//           user = await User.create({
-//             name: profile.displayName,
-//             email: profile.emails[0].value,
-//             isVerified: true,
-//             password: "oauth_google",
-//           });
-//         }
-
-//         return done(null, user);
-//       } catch (err) {
-//         return done(err, null);
-//       }
-//     }
-//   )
-// );
-
-// // ================= LINKEDIN =================
-// passport.use(
-//   new LinkedInStrategy(
-//     {
-//       clientID: process.env.LINKEDIN_CLIENT_ID,
-//       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-//       callbackURL: "/api/auth/linkedin/callback",
-//       scope: ["r_emailaddress", "r_liteprofile"],
-//     },
-//     async (accessToken, refreshToken, profile, done) => {
-//       try {
-//         const email = profile.emails[0].value;
-//         let user = await User.findOne({ email });
-
-//         if (!user) {
-//           user = await User.create({
-//             name: profile.displayName,
-//             email,
-//             isVerified: true,
-//             password: "oauth_linkedin",
-//           });
-//         }
-
-//         return done(null, user);
-//       } catch (err) {
-//         return done(err, null);
-//       }
-//     }
-//   )
-// );
-
-// module.exports = passport;
