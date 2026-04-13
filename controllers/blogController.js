d9ef81610040a7b946d2d9ff0d21791d7aca123a
rabiaquantristech <rabiaquantristech@gmail.com>
2026-04-13 16:43:21 +0500

    set checker

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -34,11 +34,16 @@ exports.getBlogBySlug = async (req, res) => {
     ).populate("author", "name");
 
     if (!blog) return res.status(404).json({ message: "Blog not found" });
+
+    console.log("Fetched Blog Data:", blog); // Check the log here
+
     res.status(200).json({ success: true, data: blog });
   } catch (error) {
+    console.error("Error fetching blog:", error);
     res.status(500).json({ message: error.message });
   }
 };
+
 
 // Admin
 // exports.adminGetBlogs = async (req, res) => {
@@ -69,15 +74,15 @@ exports.adminGetBlogs = async (req, res) => {
     if (status) query.status = status;
     if (category) query.category = category;
     if (search) query.title = { $regex: search, $options: "i" };
- 
+
     const blogs = await Blog.find(query)
       .populate("author", "name")
       .sort({ createdAt: -1 })
       .skip((Number(page) - 1) * Number(limit))
       .limit(Number(limit));
- 
+
     const total = await Blog.countDocuments(query);
- 
+
     res.status(200).json({
       success: true,
       data: blogs,
@@ -92,7 +97,7 @@ exports.adminGetBlogs = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
- 
+
 
 // exports.adminCreateBlog = async (req, res) => {
 //   try {
