9f329a326dde593bf884312470b53376471573da
rabiaquantristech <rabiaquantristech@gmail.com>
2026-04-14 13:03:51 +0500

    set cloudinary setup for image upload

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -8,6 +8,15 @@ const {
   adminUpdateBlog, adminDeleteBlog, adminPublishBlog,
   uploadImage
 } = require("../controllers/blogController.js");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
+
+const upload = multer({ storage });
 
 router.post("/upload", upload.single("image"), uploadImage);
 
