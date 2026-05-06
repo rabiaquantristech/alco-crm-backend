9229244cbaf7a075160393ffb2878b3cee7a5ed5
rabiaquantristech <rabiaquantristech@gmail.com>
2026-05-06 12:27:18 +0500

    aditlog for just admin

--- routes/auditRoute.js
+++ routes/auditRoute.js
@@ -9,7 +9,7 @@ const { protect } = require("../middlewares/authMiddleware.js");
 const { authorize } = require("../middlewares/roleMiddleware.js");
  
 // Admin & Super Admin only
-router.get("/", protect, authorize( "admin", "super_admin", "sales_manager", "finance_manager"), getAllAuditLogs);
-router.get("/:id", protect, authorize( "admin", "super_admin", "sales_manager", "finance_manager"), getAuditLogById);
+router.get("/", protect, authorize( "admin", "super_admin"), getAllAuditLogs);
+router.get("/:id", protect, authorize( "admin", "super_admin"), getAuditLogById);
  
 module.exports = router;
